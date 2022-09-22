e00aaed32816d0e216d8cfdbfe706458be1c2999
Haviv <lhaviv@3c06303e1816.ant.amazon.com>
2022-09-21 20:52:22 -0400

    create post_to_slack function and add error handling

--- lambda_function.py
+++ lambda_function.py
@@ -7,54 +7,61 @@ def lambda_handler(event, context):
     
     url = os.environ['TECH_FEED']
     headers = {
-    	"X-RapidAPI-Key": os.environ['RAPIDAPI_KEY'],
-    	"X-RapidAPI-Host": os.environ['RAPIDAPI_HOST']
+            "X-RapidAPI-Key": os.environ['RAPIDAPI_KEY'],
+            "X-RapidAPI-Host": os.environ['RAPIDAPI_HOST']
     }
 
     hdrs = {'Content-Type': 'application/json'}
     
-    req = urllib.request.Request(url, headers = headers)
-    response = urllib.request.urlopen(req)
-    data =  response.read()
-    
-    res = json.loads(data)
-    stories = str(res[0])
-    
-    response_body = {
-    	"blocks": [
-    		{
-    			"type": "section",
-    			"text": {
-    				"type": "mrkdwn",
-    				"text": ":newspaper: New Story published on 9to5 Mac"
-    			}
-    		},
-    		{
-    			"type": "divider"
-    		},
-    		{
-    			"type": "section",
-    			"text": {
-    				"type": "mrkdwn",
-    				"text": "*<https://{0}|{1}>*\n".format(res[0]["link"], res[0]["title"])
-    			},
-    			"accessory": {
-    				"type": "image",
-    				"image_url": res[0]["img"],
-    				"alt_text": "tech"
-    			}
-    		},
-    		{
-    			"type": "divider"
-    		}
-    	]
-    }
-    
-    print(stories)
-    message = { "text": stories}
-    
-    req = urllib.request.Request(os.environ['WEBHOOK_URL'], json.dumps(response_body).encode('utf-8'), headers = hdrs)
-    resp = urllib.request.urlopen(req)
-    response = resp.read()
+    try: 
+        req = urllib.request.Request(url, headers = headers)
+        response = urllib.request.urlopen(req)
+    #if there is an error
+    except urllib.error.HTTPError as e:
+        # Return code error (e.g. 404, 501, ...)
+        print('HTTPError: {}'.format(e.code))
+    else:
+        #iterate over the results
+        data =  response.read()
+        res = json.loads(data)
+        for item in reversed(res):
+          post_to_slack(item["link"], item["title"], item["img"])  
+
+
+    def post_to_slack (link, title, img): 
+        response_body = {
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": ":newspaper: New Story published on 9to5 Mac"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*<https://{0}|{1}>*\n".format(link, title)
+                    },
+                    "accessory": {
+                        "type": "image",
+                        "image_url": img,
+                        "alt_text": "tech"
+                    }
+                },
+                {
+                    "type": "divider"
+                }
+            ]
+        }
+        
+        req = urllib.request.Request(os.environ['WEBHOOK_URL'], json.dumps(response_body).encode('utf-8'), headers = hdrs)
+        resp = urllib.request.urlopen(req)
+        response = resp.read()
+        print (response)
     
     
